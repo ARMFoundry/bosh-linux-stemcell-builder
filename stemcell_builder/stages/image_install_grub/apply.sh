32c0786889c8ce19c7bf3f5902628b9ee3667776
Ruurd Keizer <ruurdk@hotmail.com>
2018-09-21 08:09:35 +0000

    cgroups kernel stuff

--- stemcell_builder/stages/image_install_grub/apply.sh
+++ stemcell_builder/stages/image_install_grub/apply.sh
@@ -120,7 +120,7 @@ GRUB_HIDDEN_TIMEOUT=0
 GRUB_HIDDEN_TIMEOUT_QUIET=true
 GRUB_TIMEOUT=10
 GRUB_DISTRIBUTOR=`lsb_release -i -s 2> /dev/null || echo Debian`
-GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"
+GRUB_CMDLINE_LINUX_DEFAULT="quiet splash cgroup_enable=memory swapaccount=1"
 GRUB_CMDLINE_LINUX=""
 EOF
       else
